a4238181583d3d6cc8bc18d761bf6595c305808c
Pilamunga <105055642+Pilamunga@users.noreply.github.com>
2022-05-10 08:44:36 -0500

    Add files via upload

---
+++ untitled8.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 10 08:39:46 2022
+
+@author: Alumno
+"""
+
+contar=input("Ingrese el # al que debo contar: ")
+contar=int(contar)
+contador=1
+while contador<=contar:
+    print(contador)
+    contador+=1
