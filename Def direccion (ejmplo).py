dabbbaf82fc8bbdb489cf9ee9a39c7047ee0330c
Pilamunga <105055642+Pilamunga@users.noreply.github.com>
2022-06-07 08:51:17 -0500

    Add files via upload

---
+++ Def direccion (ejmplo).py
@@ -0,0 +1,13 @@
+def direction(calle,barrio,cuidad):
+    print("Su direccion es: ")
+    print("Su cuidad es: ", cuidad)
+    print("El barrio de entrega es: ",barrio)
+    print("La calle de su domicilo es: Av: ",calle)
+    
+ci=input("Ingrese la cuidad")
+cl=input("Ingrese la calle de entrega")
+ba=input("Ingrese el sector de referencia para la entrega")
+
+direction(cl,ba,ci)
+
+
