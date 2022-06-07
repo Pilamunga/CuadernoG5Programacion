b80ed551d23d1da30edde4da6ad91998ad526236
Pilamunga <105055642+Pilamunga@users.noreply.github.com>
2022-06-07 08:51:47 -0500

    Add files via upload

---
+++ Def direccion(ejemplo).py
@@ -0,0 +1,8 @@
+def address(street, city, postalCode):
+    print("Su Direccion es: ", street, "St.,", city, postalCode)
+    
+s=input("Street: ")
+pC=input("Postal Code: ")
+c=input("City: ")
+
+address(s, c, pC)
