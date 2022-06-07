13353488f6026074a6ead2ee39f1f05c24c8ae8e
Pilamunga <105055642+Pilamunga@users.noreply.github.com>
2022-06-07 07:57:03 -0500

    Add files via upload

---
+++ ejemplo de funciones python .py
@@ -0,0 +1,8 @@
+print("Ingrese el siguiente valor")
+a=input()
+print("Ingrese el siguiente valor")
+b=input()
+print("Ingrese el siguiente valor")
+c=input()
+resultado=a+b+c
+print(resultado)
---
+++ uso def ..py
@@ -0,0 +1,13 @@
+def mensaje():
+     print("Ingrese su estatura: ")
+
+mensaje()
+a=input()
+mensaje()
+b=input()
+mensaje()
+c=input()
+mensaje()
+d=input()
+resultado=a+b+c+d
+print(resultado)
